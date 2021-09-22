0c40a4eeccfa3f065dbc1e3513d3aaa240bf16db
Tayrone <tayrone.bez@gmail.com>
2021-09-22 14:01:40 -0300

    01

---
+++ Calculadora/index.js
@@ -0,0 +1,26 @@
+function inse(num)
+{
+    var numero = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = numero + num;
+}
+function limpar()
+{
+    document.getElementById('res').innerHTML = "";
+}
+function sair()
+{
+    var res = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = res.substring(0, res.length -1);
+}
+function calcular()
+{
+    var res = document.getElementById('res').innerHTML;
+    if(res)
+    {
+        document.getElementById('res').innerHTML = eval(res);
+    }
+    else
+    {
+        document.getElementById('res').innerHTML = "Nada..."
+    }
+}
